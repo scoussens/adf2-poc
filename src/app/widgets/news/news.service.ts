bf4a8545867d0405e93358f11247431b29f1d91d
Seth Coussens <seth.coussens@gmail.com>
2017-02-22 12:23:46 -0600

    updated modules and fixed some errors

--- src/app/widgets/news/news.service.ts
+++ src/app/widgets/news/news.service.ts
@@ -1,19 +1,28 @@
 import { Injectable } from '@angular/core';
 
 import { Jsonp } from '@angular/http';
-import { Observable }     from 'rxjs/Observable';
+import { Observable } from 'rxjs/Observable';
 
 @Injectable()
 export class NewsService {
 
-  private static readonly BASE_URL = 'https://ajax.googleapis.com/ajax/services/feed/load?v=1.0&callback=JSONP_CALLBACK&q=';
+  private static readonly BASE_URL = 'https://api.rss2json.com/v1/api.json?rss_url=';
 
-  constructor(private jsonp: Jsonp) {}
+  constructor(private jsonp: Jsonp) { }
 
   getFeed(service: string): Observable<any> {
+    service = service.replace(/\:/g, '%3A');
+    service = service.replace(/\//g, '%2F');
+    console.info(service);
     return this.jsonp.get(NewsService.BASE_URL + service)
-      .map(res => res.json())
-      .map(data => data.responseData.feed);
+      .map(res => {
+        console.info(res);
+        res.json()
+      })
+      .map(data => {
+        //console.info(data);
+        if (data) return data;
+      });
   }
 
 }
